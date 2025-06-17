31636628a9a2900177baf024e036ba3bbe408cc4
Ali Alhebsi <ali_salem@ega.rak.ae>
2025-06-17 11:57:20 +0400

    added excs -5

---
+++ python-excs-5.py
@@ -0,0 +1,9 @@
+# Prints numbers from 1 to 10
+print("Numbers from 1 to 10:")
+for i in range(1, 11):
+    print(i)
+
+# Prints only even numbers from 1 to 20
+print("\nEven numbers from 1 to 20:")
+for i in range(2, 21, 2):
+    print(i)
