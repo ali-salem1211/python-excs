afd1ee9f2aca8b6a6cd04ec029c5db9f16ee7956
Ali Alhebsi <ali_salem@ega.rak.ae>
2025-06-18 09:33:15 +0400

    added excs -17

---
+++ python-excs-17.py
@@ -0,0 +1,11 @@
+def find_duplicates(items):
+    seen = set()
+    duplicates = set()
+    for item in items:
+        if item in seen:
+            duplicates.add(item)
+        else:
+            seen.add(item)
+    return list(duplicates)
+
+print(find_duplicates([1, 2, 3, 2, 4, 5, 1]))
